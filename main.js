eaba46744807d23ae0174c09799b9ae375d91017
mziyambi <mziyam@mtc.ca.gov>
2015-07-30 13:25:23 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//Gzip Compression of content
+var compression = require('compression');
+
+
+
+//MSQL SERVER
+// var mssql = require('mssql');
+
+//MONGODB
+// var mongojs = require('mongojs');
+
+var pages = require('./routes/pages');
+
+var app = express();
+
+//MSQL SERVER CONFIGURATION
+// var configSQL = {
+//     user: 'username',
+//     password: 'password',
+//     server: 'ip address', // You can use 'localhost\\instance' to connect to named instance
+//     database: 'database'
+
+// };
+
+// var connection = new mssql.Connection(configSQL, function(err) {
+//     if (err)
+//         console.log(err);
+// });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Enables CORS
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, *');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+
+//Check to see if request requires compression or not
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false;
+    }
+
+    // fallback to standard filter function
+    return compression.filter(req, res);
+}
+
+//Compress all static content
+app.use(compression({
+    filter: shouldCompress
+}));
+
+//Load app modules
+app.use(enableCORS);
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//ROUTES
+app.use('/',pages);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
