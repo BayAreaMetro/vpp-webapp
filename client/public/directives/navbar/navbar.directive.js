32ca57adb1c8e92a7589cab04e8b4cf62ec2b78e
Kearey Smith <ksmith@mtc.ca.gov>
2015-08-26 00:25:48 -0700

    not sure about this commit

--- client/public/directives/navbar/navbar.directive.js
+++ client/public/directives/navbar/navbar.directive.js
@@ -1,17 +1,19 @@
 'use strict';
 
 angular.module('vppApp')
-	.directive("navTemplate", function () {
-		return {
-			restrict: 'EA',
-			scope: {
-				snippets: '='
-			},
-			templateUrl: 'navbar/navbar.directive.html',
-			link: function($scope) {
+    .directive("navTemplate", function () {
+        return {
+            restrict: 'EA',
+            scope: {
+                snippets: '='
+            },
+            templateUrl: 'navbar/navbar.directive.html',
+            link: function ($scope) {
 
-				// Placeholder
-				$scope.user = { name: 'me' };
-			}
-		};
-	});
+                // Placeholder
+                $scope.user = {
+                    name: 'What is this?'
+                };
+            }
+        };
+    });
