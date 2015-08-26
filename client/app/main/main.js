22e0fb1b52aea5ece31460be0af3b68fe19c5c77
Kearey Smith <ksmith@mtc.ca.gov>
2015-08-26 13:20:48 -0700

    Updating Map Content

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -18,60 +18,87 @@ angular.module('vppApp')
                 templateUrl: 'public/templates/mapping/mapping.html',
                 controller: 'MapCtrl',
                 resolve: {
-		            promiseObj: function ($q, $rootScope, wish) {
-		                var deferred = $q.defer(),
-		                    deps = {
-		                        Map: 'esri/map',
-		                        FeatureLayer: 'esri/layers/FeatureLayer',
-		                        InfoTemplate: 'esri/InfoTemplate',
-		                        SimpleFillSymbol: 'esri/symbols/SimpleFillSymbol',
-		                        SimpleRenderer: 'esri/renderers/SimpleRenderer',
-		                        SimpleMarkerSymbol: 'esri/symbols/SimpleMarkerSymbol',
-		                        ScaleDependentRenderer: 'esri/renderers/ScaleDependentRenderer',
-		                        Color: 'dojo/_base/Color'
-		                    };
-		
-		                wish.loadDependencies(deps, function () {
-		                    deferred.resolve();
-		                    if (!$rootScope.$$phase) {
-		                        $rootScope.$apply();
-		                    }
-		                });
-		
-		                return deferred.promise;
-		            }
-		        }
+                    promiseObj: function ($q, $rootScope, wish) {
+                        var deferred = $q.defer(),
+                            deps = {
+
+                                //ESRI Map
+                                Map: 'esri/map',
+                                esriConfig: 'esri/config',
+                                //Map Layer Tools
+                                FeatureLayer: 'esri/layers/FeatureLayer',
+                                InfoTemplate: 'esri/InfoTemplate',
+                                //Map Popup Tools
+                                Popup: 'esri/dijit/Popup',
+                                PopupTemplate: 'esri/dijit/PopupTemplate',
+                                //Symbol Tools
+                                SimpleFillSymbol: 'esri/symbols/SimpleFillSymbol',
+                                SimpleMarkerSymbol: 'esri/symbols/SimpleMarkerSymbol',
+                                SimpleLineSymbol: 'esri/symbols/SimpleLineSymbol',
+                                TextSymbol: 'esri/symbols/TextSymbol',
+                                //dojo Stuff
+                                Color: 'dojo/_base/Color',
+                                parser: 'dojo/parser',
+                                GreySkies: 'dojox/charting/themes/GreySkies',
+                                number: 'dojo/number',
+                                dom: 'dojo/dom',
+                                on: 'dojo/on',
+                                query: 'dojo/query',
+                                domStyle: 'dojo/dom-style',
+                                domConstruct: 'dojo/dom-construct',
+                                domReady: 'dojo/domReady',
+                                //Services
+                                GeometryService: 'esri/tasks/GeometryService',
+                                Extent: 'esri/geometry/Extent',
+                                //Renderers
+                                HeatmapRenderer: 'esri/renderers/HeatmapRenderer',
+                                ClassBreaksRenderer: 'esri/renderers/ClassBreaksRenderer',
+                                SimpleRenderer: 'esri/renderers/SimpleRenderer',
+                                ScaleDependentRenderer: 'esri/renderers/ScaleDependentRenderer'
+                            };
+
+
+                        wish.loadDependencies(deps, function () {
+                            deferred.resolve();
+                            if (!$rootScope.$$phase) {
+                                $rootScope.$apply();
+                            }
+                        });
+
+                        return deferred.promise;
+                    }
+                }
             });
     })
     .service('wish', function () {
-	
-	    // it's not require... it's a wish?
-	    var wish = {};
-	
-	    function _loadDependencies(deps, next) {
-	        var reqArr = _.values(deps),
-	            keysArr = _.keys(deps);
-	
-	        // use the dojo require (required by arcgis + dojo) && save refs
-	        // to required obs
-	        require(reqArr, function () {
-	            var args = arguments;
-	
-	            _.each(keysArr, function (name, idx) {
-	                wish[name] = args[idx];
-	            });
-	
-	            next();
-	        });
-	    }
-	
-	    return {
-	        loadDependencies: function (deps, next) {
-	            _loadDependencies(deps, next);
-	        },
-	
-	        get: function () {
-	            return wish;
-	        }
-	    };
-	});
+
+        // it's not require... it's a wish?
+        var wish = {};
+
+        function _loadDependencies(deps, next) {
+            var reqArr = _.values(deps),
+                keysArr = _.keys(deps);
+
+            // use the dojo require (required by arcgis + dojo) && save refs
+            // to required obs
+            require(reqArr, function () {
+                var args = arguments;
+
+                _.each(keysArr, function (name, idx) {
+                    wish[name] = args[idx];
+                });
+
+                next();
+            });
+        }
+
+        return {
+            loadDependencies: function (deps, next) {
+                _loadDependencies(deps, next);
+            },
+
+            get: function () {
+                return wish;
+            }
+        };
+    });
--- client/public/directives/map/map.controller.js
+++ client/public/directives/map/map.controller.js
@@ -4,14 +4,32 @@ angular.module('vppApp')
     .controller('MapCtrl', function ($rootScope, $scope, wish) {
 
         var w = wish.get(),
-            greenFill = new w.Color([133, 197, 133, 0.75]),
-            greenOutline = new w.Color([133, 197, 133, 0.25]),
             layer,
             markerSym,
             renderer1,
             renderer2,
+            sls,
+            sfs,
+            popup,
+            popupOptions,
 
-            CROPS_URL = 'http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/USA_County_Crops_2007/FeatureServer/0';
+            MAP_URL = 'http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/USA_County_Crops_2007/FeatureServer/0';
+
+        w.parser.parse();
+        w.esriConfig.defaults.geometryService = new w.GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
+
+
+        sls = new w.SimpleLineSymbol("solid", new w.Color("#444444"), 3);
+        sfs = new w.SimpleFillSymbol("solid", sls, new w.Color([68, 68, 68, 0.25]));
+
+        popupOptions = {
+            fillSymbol: sfs,
+            marginLeft: "20",
+            marginTop: "20"
+        };
+
+        //create a popup to replace the map's info window
+        popup = new w.Popup(popupOptions, w.domConstruct.create("div"));
 
         $scope.map = new w.Map('map', {
             center: [-122.23, 37.75],
@@ -19,46 +37,45 @@ angular.module('vppApp')
             basemap: 'topo'
         });
 
-        layer = new w.FeatureLayer(CROPS_URL, {
-            outFields: ['STATE', 'COUNTY', 'M086_07', 'AREA'],
+        layer = new w.FeatureLayer(MAP_URL, {
+            outFields: ['*'],
             infoTemplate: new w.InfoTemplate('${COUNTY}, ${STATE}', '<div style="font: 18px Segoe UI">The percentage of the area of the county that represents farmland is <b>${M086_07}%</b>.</div>')
         });
         layer.setDefinitionExpression('AREA>0.01 and M086_07>0');
 
 
-        markerSym = new w.SimpleMarkerSymbol();
-        markerSym.setColor(greenFill);
-        markerSym.setOutline(markerSym.outline.setColor(greenOutline));
-        renderer1 = new w.SimpleRenderer(markerSym);
-        renderer1.setProportionalSymbolInfo({
-            field: 'M086_07',
-            minSize: 1,
-            maxSize: 10,
-            minDataValue: 0,
-            maxDataValue: 100
-        });
+//        markerSym = new w.SimpleMarkerSymbol();
+//        markerSym.setColor(greenFill);
+//        markerSym.setOutline(markerSym.outline.setColor(greenOutline));
+//        renderer1 = new w.SimpleRenderer(markerSym);
+//        renderer1.setProportionalSymbolInfo({
+//            field: 'M086_07',
+//            minSize: 1,
+//            maxSize: 10,
+//            minDataValue: 0,
+//            maxDataValue: 100
+//        });
 
         //for the second renderer increase the dot sizes and set a backgroundFillSymbol
-        renderer2 = new w.SimpleRenderer(markerSym);
-        renderer2.setProportionalSymbolInfo({
-            field: 'M086_07',
-            minSize: 5,
-            maxSize: 15,
-            minDataValue: 0,
-            maxDataValue: 100
-        });
+//        renderer2 = new w.SimpleRenderer(markerSym);
+//        renderer2.setProportionalSymbolInfo({
+//            field: 'M086_07',
+//            minSize: 5,
+//            maxSize: 15,
+//            minDataValue: 0,
+//            maxDataValue: 100
+//        });
 
-        layer.setRenderer(new w.ScaleDependentRenderer({
-            rendererInfos: [{
-                'renderer': renderer1,
-                'minScale': 50000000,
-                'maxScale': 10000000
-         }, {
-                'renderer': renderer2,
-                'minScale': 0,
-                'maxScale': 5000000
-         }]
-        }));
-
+//        layer.setRenderer(new w.ScaleDependentRenderer({
+                //            rendererInfos: [{
+                //                'renderer': renderer1,
+                //                'minScale': 50000000,
+                //                'maxScale': 10000000
+                //         }, {
+                //                'renderer': renderer2,
+                //                'minScale': 0,
+                //                'maxScale': 5000000
+                //         }]
+                //        }));
         $scope.map.addLayer(layer);
     });
