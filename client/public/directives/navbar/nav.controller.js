d60eab11fa760bdbc9dd9ef9cc8382e474bee06e
Gilbert frausto <gilbertfrausto@gmail.com>
2015-09-09 17:02:49 -0700

    [Research] Nav and content
    Added styles to Nav and added research employee content

--- client/public/directives/navbar/nav.controller.js
+++ client/public/directives/navbar/nav.controller.js
@@ -6,6 +6,20 @@ angular.module('vppApp')
     	function ($scope) {
 	    	
 	    	//Show nav
+	    	$('.show-inner-list').hover(function(){
+		    	$(this).closest('ul').addClass('show-inner');
+	    	}, function(){
+		    	$(this).removeClass('show-inner');
+	    	});
+	    	
+	    	//Show Inner nav
+	    	$('.vpp-links-li').hover(function(){
+		    	$(this).addClass('show-inner-nested');
+	    	}, function(){
+		    	$(this).removeClass('show-inner-nested');
+	    	});
+	    	
+	    	//ng show and hide
 			$scope.showInnerLinks = function(event){
 				event.target.classList.add('show-inner');
 				//$(event.target).addClass('show-inner');
@@ -15,6 +29,20 @@ angular.module('vppApp')
 			//Hide nav
 			$scope.hideInnerLinks = function(event){
 				event.target.classList.remove('show-inner');
+				//$(".nav-list").removeClass('show-inner');
+				console.log('hide');
+			}
+			
+			//Show nav
+			$scope.showInnerLinksNested = function(event){
+				event.target.classList.add('show-inner-nested');
+				//$(event.target).addClass('show-inner');
+				console.log('show');
+			}
+			
+			//Hide nav
+			$scope.hideInnerLinksNested = function(event){
+				event.target.classList.remove('show-inner-nested');
 				//$(".nav-list").removeClass('show-inner');
 				console.log('hide');
 			}
