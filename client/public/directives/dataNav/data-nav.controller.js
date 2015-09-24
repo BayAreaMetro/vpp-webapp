fd04915f366f8b884a874eae6fb3f998bb299250
Gilbert frausto <gilbertfrausto@gmail.com>
2015-09-24 13:41:40 -0700

    [Data] Showing data table
    Fixing showing and hiding the data tables

--- client/public/directives/dataNav/data-nav.controller.js
+++ client/public/directives/dataNav/data-nav.controller.js
@@ -1,32 +1,32 @@
 'use strict';
 angular.module('vppApp')
     .controller('DataCtrl', [
-  '$scope',
-  function () {
-            //console.log('Data Controller');
-            //Page Controls
-            $('.divControl').click(function () {
-                $('.divFade').each(function () {
-                    if ($('.divFade').css("visibility") == "hidden") {
-                        // handle non visible state
-                    } else {
-                        // handle visible state
-                        $('.divFade').fadeOut(0);
-                    }
-                });
-            });
-            $("#dbInventoryMI").click(function () {
-                $("#InventorySummary").fadeIn(500);
-            });
-            $('#dbSummaryMI').click(function () {
-                $("#DataSummary").fadeIn(500);
-            });
-            $('#dbOccupancyMI').click(function () {
-                $("#OccupancySummary").fadeIn(500);
-            });
-            $('#dbOverviewMI').click(function () {
-                //console.clear();
-                //console.log("Data Overview Loading...")
-            });
-  }
+	'$scope',
+	function () {
+	        //console.log('Data Controller');
+	        //Page Controls
+	        $('.divControl').click(function () {
+	            $('.divFade').each(function () {
+	                if ($('.divFade').css("visibility") == "hidden") {
+	                    // handle non visible state
+	                } else {
+	                    // handle visible state
+	                    $('.divFade').fadeOut(0);
+	                }
+	            });
+	        });
+	        $("#dbInventoryMI").click(function () {
+	            $("#InventorySummary").fadeIn(500);
+	        });
+	        $('#dbSummaryMI').click(function () {
+	            $("#DataSummary").fadeIn(500);
+	        });
+	        $('#dbOccupancyMI').click(function () {
+	            $("#OccupancySummary").fadeIn(500);
+	        });
+	        $('#dbOverviewMI').click(function () {
+	            //console.clear();
+	            //console.log("Data Overview Loading...")
+	        });
+	}
  ]);
