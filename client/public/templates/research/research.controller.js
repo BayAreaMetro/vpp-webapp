b41b5c9c818497787f51a2844a539b5a861c8c96
Gilbert frausto <gilbertfrausto@gmail.com>
2015-09-24 12:03:51 -0700

    [Research] Rebuilt nav

--- client/public/templates/research/research.controller.js
+++ client/public/templates/research/research.controller.js
@@ -2,47 +2,59 @@
 
 angular.module('vppApp')
     .controller('ResearchCtrl', [
-  '$scope',
+	'$scope',
     '$http',
-  function ($scope) {
-            //Page Controls
-            $('.divControl').click(function () {
-                $(".divFade").each(function () {
-                    if ($('.divFade').css("visibility") == "hidden") {
-                        // handle non visible state
-                    } else {
-                        // handle visible state
-                        $('.divFade').fadeOut(10);
-                    }
-                });
-            });
-
-            $('#resEmployeeProgramMI').click(function () {
-                $("#EmployeePrograms").fadeIn(1000);
-            });
-
-            $('#resRegPricingPolMI').click(function () {
-                $("#RegionalPricingPolicies").fadeIn(1000);
-            });
-
-            $('#resEnactingPricingPolicies').click(function () {
-                $("#EnactingPricingPolicies").fadeIn(1000);
-            });
-            $('#resResources').click(function () {
-                $("#Resources").fadeIn(1000);
-            });
-            $('#resPolicyQuestions').click(function () {
-                $("#PolicyQuestions").fadeIn(1000);
-            });
-            $('#resSupplyDemand').click(function () {
-                $("#SupplyDemand").fadeIn(1000);
-            });
-            $('#resParkingReqmntsUnbundling').click(function () {
-                $("#ParkingReqmntsUnbundling").fadeIn(1000);
-            });
-            $('#resTravelModelOneAnalysis').click(function () {
-                $("#TravelModelOneAnalysis").fadeIn(1000);
-            });
-
+	function ($scope) {
+	    //Page Controls
+	    $('.divControl').click(function () {
+	        $(".divFade").each(function () {
+	            if ($('.divFade').css("visibility") == "hidden") {
+	                // handle non visible state
+	            } else {
+	                // handle visible state
+	                $('.divFade').fadeOut(10);
+	            }
+	        });
+	    });
+	
+	    $('#resEmployeeProgramMI').click(function () {
+	        $("#EmployeePrograms").fadeIn(1000);
+	    });
+	
+	    $('#resRegPricingPolMI').click(function () {
+	        $("#RegionalPricingPolicies").fadeIn(1000);
+	    });
+	
+	    $('#resEnactingPricingPolicies').click(function () {
+	        $("#EnactingPricingPolicies").fadeIn(1000);
+	    });
+	    $('#resResources').click(function () {
+	        $("#Resources").fadeIn(1000);
+	    });
+	    $('#resPolicyQuestions').click(function () {
+	        $("#PolicyQuestions").fadeIn(1000);
+	    });
+	    $('#resSupplyDemand').click(function () {
+	        $("#SupplyDemand").fadeIn(1000);
+	    });
+	    $('#resParkingReqmntsUnbundling').click(function () {
+	        $("#ParkingReqmntsUnbundling").fadeIn(1000);
+	    });
+	    $('#resTravelModelOneAnalysis').click(function () {
+	        $("#TravelModelOneAnalysis").fadeIn(1000);
+	    });
+		
+		$scope.init = function(){
+			$(".inner-nested").hover(function(){
+				$('.inner-ul').addClass('active');
+				$('.inner-nested').addClass('active');
+			},function(){
+				$('.inner-ul').removeClass('active');
+				$('.inner-nested').removeClass('active');
+			});
+			console.log('reseach');
+		};
+		
+		$scope.init();
   }
  ]);
