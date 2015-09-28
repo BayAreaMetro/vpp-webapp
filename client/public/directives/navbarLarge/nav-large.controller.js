f74c5bb212ead1c9c488c54aea40f81328e0bae0
Gilbert frausto <gilbertfrausto@gmail.com>
2015-09-28 12:42:40 -0700

    [Nav] Research
    Added new logic to show and hide the div’s when a nested link is pressed

--- client/public/directives/navbarLarge/nav-large.controller.js
+++ client/public/directives/navbarLarge/nav-large.controller.js
@@ -42,6 +42,45 @@ angular.module('vppApp')
 				$('.navbar-nav > li > a').removeClass('active');
 				$(e.target).addClass("active");
 			};
+			
+			$scope.inner = function(event){
+				$scope.overview = false;
+				
+				//Get attr
+				$scope.getLocation = $(event.target).attr("location");
+				$scope.getElement = document.getElementById($scope.location);
+				$scope.makeId = '"#' + $scope.getLocation +'"';
+				
+				//Hide divs
+				$('.divFade').hide();
+				
+				if($scope.getLocation === "policy-overview"){
+					
+					//Fadein section
+					$("#policy-overview").fadeIn();
+				} else if($scope.getLocation === "policy-urban"){
+					
+					//Fadein section
+					$("#policy-urban").fadeIn();
+				} else if($scope.getLocation === "policy-travel"){
+					
+					$("#policy-travel").fadeIn();
+				} else if($scope.getLocation === "policy-key"){
+					
+					$("#policy-key").fadeIn();
+				} else if($scope.getLocation === "supply-overview"){
+					
+					$("#supply-overview").fadeIn();
+				} else if($scope.getLocation === "supply-parking"){
+					
+					$("#supply-parking").fadeIn();
+				} else if($scope.getLocation === "TravelModelOneAnalysis"){
+					
+					$("#TravelModelOneAnalysis").fadeIn();
+				}
+				console.log($scope.makeId);
+				
+			}
     	}
     ]	
 );
